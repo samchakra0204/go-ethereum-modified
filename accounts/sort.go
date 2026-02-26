2620663999f24905c819ea42ebca60a12c25b006
Samag Chakrabarti <samagchakrabarti@gmail.com>
2026-02-26 10:35:40 +0530

    Refactor AccountsByURL and WalletsByURL Less() for readability

--- accounts/sort.go
+++ accounts/sort.go
@@ -21,11 +21,19 @@ type AccountsByURL []Account
 
 func (a AccountsByURL) Len() int           { return len(a) }
 func (a AccountsByURL) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a AccountsByURL) Less(i, j int) bool { return a[i].URL.Cmp(a[j].URL) < 0 }
+func (a AccountsByURL) Less(i, j int) bool {
+	ui := a[i].URL
+	uj := a[j].URL
+	return ui.Cmp(uj) < 0
+}
 
 // WalletsByURL implements sort.Interface for []Wallet based on the URL field.
 type WalletsByURL []Wallet
 
 func (w WalletsByURL) Len() int           { return len(w) }
 func (w WalletsByURL) Swap(i, j int)      { w[i], w[j] = w[j], w[i] }
-func (w WalletsByURL) Less(i, j int) bool { return w[i].URL().Cmp(w[j].URL()) < 0 }
+func (w WalletsByURL) Less(i, j int) bool {
+	ui := w[i].URL()
+	uj := w[j].URL()
+	return ui.Cmp(uj) < 0
+}

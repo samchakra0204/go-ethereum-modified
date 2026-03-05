4bb9a411d68ec48b27e6e71276d7441b4f546261
Samag Chakrabarti <samagchakrabarti@gmail.com>
2026-03-05 13:36:50 +0530

    Minor optimization in AccountsByURL and WalletsByURL Less()

--- accounts/sort.go
+++ accounts/sort.go
@@ -16,24 +16,26 @@
 
 package accounts
 
-// AccountsByURL implements sort.Interface for []Account based on the URL field.
 type AccountsByURL []Account
 
-func (a AccountsByURL) Len() int           { return len(a) }
-func (a AccountsByURL) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a AccountsByURL) Len() int      { return len(a) }
+func (a AccountsByURL) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a AccountsByURL) Less(i, j int) bool {
-	ui := a[i].URL
-	uj := a[j].URL
+	ui, uj := a[i].URL, a[j].URL
+	if ui == uj {
+		return false
+	}
 	return ui.Cmp(uj) < 0
 }
 
-// WalletsByURL implements sort.Interface for []Wallet based on the URL field.
 type WalletsByURL []Wallet
 
-func (w WalletsByURL) Len() int           { return len(w) }
-func (w WalletsByURL) Swap(i, j int)      { w[i], w[j] = w[j], w[i] }
+func (w WalletsByURL) Len() int      { return len(w) }
+func (w WalletsByURL) Swap(i, j int) { w[i], w[j] = w[j], w[i] }
 func (w WalletsByURL) Less(i, j int) bool {
-	ui := w[i].URL()
-	uj := w[j].URL()
+	ui, uj := w[i].URL(), w[j].URL()
+	if ui == uj {
+		return false
+	}
 	return ui.Cmp(uj) < 0
 }
